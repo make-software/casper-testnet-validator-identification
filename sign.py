01dfd68843d754a954b3b521d6350fa723e038e3
Muhammet Kara <muhammetk@gmail.com>
2021-07-07 14:48:57 +0300

    Add sign with PEM ability

--- sign.py
+++ sign.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python
+
+import ecc_ed25519
+import base64
+import sys, getopt
+
+msg = ""
+secret_key_path = "validator_keys/signer_secret_key.pem"
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hm:k:",["message=","secretkey="])
+except getopt.GetoptError:
+    print('sign.py -m YOURMESSAGE -k PATH-TO-YOUR-SECRET-KEY')
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == '-h':
+        print('sign.py -m YOURMESSAGE -k PATH-TO-YOUR-SECRET-KEY')
+        sys.exit()
+    elif opt in ("-m", "--message"):
+        msg = arg
+    elif opt in ("-k", "--secretkey"):
+        secret_key_path = arg
+
+if msg == "":
+    print("Message can't be empty!")
+    sys.exit()
+
+msg_as_bytes = str.encode(msg)
+signature = ecc_ed25519.get_signature_from_pem_file(msg_as_bytes, secret_key_path)
+encoded_signature = base64.b64encode(signature)
+
+print("Your message:\n", msg)
+print("Signature for your message:\n", encoded_signature)
