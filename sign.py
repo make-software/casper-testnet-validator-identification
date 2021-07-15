71182efaa45e7fb8a284da68de4088e6a7696f1a
Muhammet Kara <muhammetk@gmail.com>
2021-07-15 17:03:25 +0300

    No more base64

--- sign.py
+++ sign.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import ecc_ed25519
-import base64
 import sys, getopt
 
 msg = ""
--- verify.py
+++ verify.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import ecc_ed25519
-import base64
 import sys, getopt
 from cryptography.hazmat.primitives.asymmetric import ed25519
 
@@ -10,14 +9,14 @@ public_key_hex = ""
 encoded_signature = ""
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"hm:k:s:",["message=","publickeyhex=", "b64signature="])
+    opts, args = getopt.getopt(sys.argv[1:],"hm:k:s:",["message=","publickeyhex=", "signature="])
 except getopt.GetoptError:
-    print('verify.py -m YOURMESSAGE -k PUBLIC-KEY-HEX -s SIGNATURE-BASE-64')
+    print('verify.py -m YOURMESSAGE -k PUBLIC-KEY-HEX -s SIGNATURE')
     sys.exit(2)
 
 for opt, arg in opts:
     if opt == '-h':
-        print('verify.py -m YOURMESSAGE -k PUBLIC-KEY-HEX -s SIGNATURE-BASE-64')
+        print('verify.py -m YOURMESSAGE -k PUBLIC-KEY-HEX -s SIGNATURE')
         sys.exit()
     elif opt in ("-m", "--message"):
         msg = arg
@@ -28,13 +27,13 @@ for opt, arg in opts:
         public_key_hex = public_key_hex[2:]
         public_bytes_from_hex = bytes.fromhex(public_key_hex)
         loaded_public_key = ed25519.Ed25519PublicKey.from_public_bytes(public_bytes_from_hex)
-    elif opt in ("-s", "--b64signature"):
+    elif opt in ("-s", "--signature"):
         encoded_signature = arg
         signature = bytes.fromhex(encoded_signature)
 
 if msg == "" or public_key_hex == "" or encoded_signature == "":
     print("All arguments are required!")
-    print('./verify.py -m YOURMESSAGE -k PUBLIC-KEY-HEX -s SIGNATURE-BASE-64')
+    print('./verify.py -m YOURMESSAGE -k PUBLIC-KEY-HEX -s SIGNATURE')
     sys.exit()
 
 print("Your message:\n", msg)
